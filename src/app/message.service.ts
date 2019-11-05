643383ddba0b67e273132e830f26909fc1c10d33
harik31197 <52651351+harik31197@users.noreply.github.com>
2019-11-05 12:58:35 +0530

    Update message.service.ts

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -5,11 +5,12 @@ import { Injectable } from '@angular/core';
 })
 export class MessageService {
 messages: string[] = [];
+
 add(message:string){
   this.messages.push(message);
 }
 clear(){
   this.messages = [];
 }
-  constructor() { }
+ 
 }
