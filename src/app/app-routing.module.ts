4cce8f7776be5297dbcdf1fdd1eb0299138c025b
harik31197 <52651351+harik31197@users.noreply.github.com>
2019-11-05 12:55:10 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { EmployeesComponent } from './employees/employees.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {EmployeeDetailComponent} from './employee-detail/employee-detail.component';
 import {LoginComponent} from './login/login.component';
+import {UserviewComponent} from './userview/userview.component'
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   {path: 'dashboard' , component: DashboardComponent},
   {path:'employees', component: EmployeesComponent},
   {path: 'detail/:id', component:EmployeeDetailComponent},
-  {path:'login',component: LoginComponent}
+  {path:'login',component: LoginComponent},
+  {path:'user',component: UserviewComponent}
   
 ];
 
