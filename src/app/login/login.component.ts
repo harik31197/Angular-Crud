7bc7ec4300bc690d45bb26ec506a5f4cff2dca0b
harik31197 <52651351+harik31197@users.noreply.github.com>
2019-11-05 12:58:27 +0530

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NgForm} from '@angular/forms';
+import { Router } from '@angular/router';
+import {Login} from '../loginInfo';
+import {Employee} from '../employee';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  
 
-  constructor() { }
+  empLogin: Login = {
+    username : '',
+    password : ''
+  }
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+
+  onSubmit(loginpage:NgForm) {
+    if((this.empLogin.username === 'ganguly') && (this.empLogin.password === 'president'))
+    {
+    console.log("dfsf");
+    localStorage.setItem('username','ganguly');
+    this.router.navigate(["dashboard"]);
+   
+  }
+
+  else if((this.empLogin.username === 'kohli') && (this.empLogin.password === 'captain'))
+  {
+    console.log("Captain is in");
+    localStorage.setItem('username','kohli');
+    this.router.navigate(["user"]);
+  }
+  else
+ {
+   console.log("gone");
+    alert("Invalid credentials");
+ }
+}
 }
